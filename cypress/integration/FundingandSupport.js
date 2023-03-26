03de6d32f271306426ca2b33f22ff02ce495f480
Siva <to.sivashankar@gmail.com>
2023-03-26 18:02:29 +1300

    adding only positive scenario

--- cypress/integration/FundingandSupport.js
+++ cypress/integration/FundingandSupport.js
@@ -16,20 +16,20 @@ describe('Funding and supprt model test suite',function(){
         cy.get('.funding-results-count').should('have.text','6 Results');
     })
 
-    it('Failing test cases -Housing/Service provider',function(){
-        cy.visit("https://www.hud.govt.nz/")
-        cy.contains(' Looking for funding & support? ')
-        cy.contains('I am')
-        cy.get('.last-word-and-arrow').should('exist').click()    
-        cy.get('ul').eq(0).find('li').should('have.length', 7)
-        cy.get('ul').eq(0).find('li').eq(1).click()
-        cy.contains('looking for ')
-        cy.contains('select').click()
-        cy.get('ul').eq(0).find('li').should('have.length', 3)
-        cy.get('ul').eq(0).find('li').eq(1).click()
-        cy.get('.button').contains('What are my options?').click()
+    // it('Failing test cases -Housing/Service provider',function(){
+    //     cy.visit("https://www.hud.govt.nz/")
+    //     cy.contains(' Looking for funding & support? ')
+    //     cy.contains('I am')
+    //     cy.get('.last-word-and-arrow').should('exist').click()    
+    //     cy.get('ul').eq(0).find('li').should('have.length', 7)
+    //     cy.get('ul').eq(0).find('li').eq(1).click()
+    //     cy.contains('looking for ')
+    //     cy.contains('select').click()
+    //     cy.get('ul').eq(0).find('li').should('have.length', 3)
+    //     cy.get('ul').eq(0).find('li').eq(1).click()
+    //     cy.get('.button').contains('What are my options?').click()
 
-         // Assert  the funding result count 
-        cy.get('.funding-results-count').should('have.text','6 Results');
-    })
+    //      // Assert  the funding result count 
+    //     cy.get('.funding-results-count').should('have.text','6 Results');
+    // })
 })
